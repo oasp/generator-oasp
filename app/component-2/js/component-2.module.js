100c182a1aebd1d4e9837c19c3d59a3be5fa880d
Tomasz Bialecki <tomasz.bialecki@capgemini.com>
2015-01-23 16:12:43 +0100

    intex.html fixed

--- app/component-2/js/component-2.module.js
+++ app/component-2/js/component-2.module.js
@@ -1,4 +1,4 @@
-angular.module('app.component2', ['ngRoute', 'app.component-2.templates'])
+angular.module('app.component2', ['ngRoute', 'app.component2.templates'])
     .config(function ($routeProvider) {
         'use strict';
         $routeProvider.when('/component-2/dialog-b', {templateUrl: 'component-2/html/dialog-b.html'});
