9926f5bbfe1690e3ed3ac4ef1b8a6da90f74748c
Marek Matczak <marek.matczak@capgemini.com>
2015-02-20 14:17:25 +0100

    removed duplicated content

--- app/app.module.js
+++
@@ -1,5 +0,0 @@
-angular.module('app', ['ngRoute',  'app.main', 'app.component1', 'app.component2'])
-    .config(function ($locationProvider) {
-        'use strict';
-        $locationProvider.html5Mode(false);
-    });
--- app/component-1/js/component-1.module.js
+++
@@ -1,7 +0,0 @@
-angular.module('app.component1', ['ngRoute'])
-    .config(function ($routeProvider) {
-        'use strict';
-        $routeProvider.when('/component-1/dialog-a', {templateUrl: 'component-1/html/dialog-a.html'});
-    });
-
-
